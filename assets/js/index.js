e5692a6218a2025923877214f87d03f3b56782f9
2533404880@qq.com <mengxiang748>
2022-05-21 16:47:06 +0800

    更新了user

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,51 @@
-$(function () {
-    // 调用 getUserInfo 函数获取用户基本信息
-    getUserInfo();
-})
-const layer = layui.layer;
 // 获取用户基本信息
-function getUserInfo() {
-    $.ajax({
-        type: "GET",
-        url: "/my/userinfo",
-        headers: {
-            Authorization: localStorage.getItem("token"),
-        },
-        success: (res) => {
-            if (res.status !== 0) return layer.msg("数据请求失败！");
-            layer.msg('成功')
-            // 调用 renderAvatar 渲染用户头像
-            // renderAvatar(res.data);
-        },
+$(function () {
+    getUserInfo();
+
+    const layer = layui.layer;
+    // 退出登录
+    $("#btnLogout").click(() => {
+        layer.confirm(
+            "确定退出登录？", {
+                icon: 3,
+                title: ""
+            },
+            function (index) {
+                // 清空本地存储里面的 token
+                localStorage.removeItem("token");
+                // 重新跳转到登录页面
+                location.href = "/login.html";
+            }
+        );
     });
-};
+});
+
+const layer = layui.layer;
+
+$.ajax({
+    type: "GET",
+    url: "/my/userinfo",
+    // headers: {
+    //   Authorization: localStorage.getItem("token"),
+    // },
+    success: (res) => {
+        if (res.status !== 0) return layer.msg("数据请求失败！");
+        console.log(res);
+        // 调用 renderAvatar 渲染用户头像
+        renderAvatar(res.data);
+    },
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    complete: (res) => {
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //  强制清空 token
+            localStorage.removeItem("token");
+            // 强制跳转到登录页面
+            location.href = "/login.html"
+        }
+    },
+});
+
 // 渲染用户头像
 const renderAvatar = (user) => {
     // 获取用户名字
@@ -34,6 +61,13 @@ const renderAvatar = (user) => {
         // 渲染文本头像
         $(".layui-nav-img").hide();
         let firstName = name[0].toUpperCase();
-        $(".text-avatar").html(firstName).show();
+        $(".text-avatar").html(firstName);
     }
 };
+
+
+
+
+
+// 调用 getUserInfo 函数获取用户基本信息
+getUserInfo();
