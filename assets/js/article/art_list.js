f21a118d134b96b8913373b96f56b1c6fc669aa6
2533404880@qq.com <mengxiang748>
2022-05-22 19:23:42 +0800

    完成文章管理相关功能的开发

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,9 +1,11 @@
 $(function () {
+    const layer = layui.layer;
+    const laypage = layui.laypage;
     // 定义一个查询的参数对象，将来请求数据的时候，
     // 需要将请求参数对象提交到服务器
     const q = {
         pagenum: 1, // 页码值，默认请求第一页的数据
-        pagesize: 20, // 每页显示几条数据，默认每页显示2条
+        pagesize: 2, // 每页显示几条数据，默认每页显示2条
         cate_id: "", // 文章分类的 Id
         state: "", // 文章的发布状态
     };
@@ -60,6 +62,7 @@ $(function () {
                 if (res.status !== 0) {
                     return layer.msg("获取分类数据失败！");
                 }
+                // layer.msg("获取分类数据成功！");
                 // 调用模板引擎渲染分类的可选项
                 var htmlStr = template("tpl-cate", res);
                 $("[name=cate_id]").html(htmlStr);
@@ -82,11 +85,6 @@ $(function () {
         // 根据最新的筛选条件，重新渲染表格的数据
         initTable()
     })
-
-    // 定义渲染分页方法
-    function renderPage(total) {
-        console.log(total);
-    }
 
     function initTable() {
         $.ajax({
@@ -136,6 +134,7 @@ $(function () {
             }
         })
     }
+
 
     $('tbody').on('click', '.btn-delete', function () {
         // 获取删除按钮的个数
